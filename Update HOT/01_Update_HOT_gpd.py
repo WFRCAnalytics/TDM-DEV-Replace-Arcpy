e535663c91bfe322536aa7e9859c96b013746bc1
Chris Day <christophersday@gmail.com>
2023-07-19 10:46:20 -0600

    Clean up scripts

--- Update HOT/01_Update_HOT_gpd.py
+++ Update HOT/01_Update_HOT_gpd.py
@@ -1,61 +1,56 @@
-# Author: Justin Culp coded and Andy Li added Toll zone functions -- Updated by Josh Reynolds and Chris Day
-# Date: 12/06/2016 -- Updated 06/01/2023
+# Author: Justin Culp & Andy Li 
+# Updated by Josh Reynolds & Chris Day
+# Date Edited:        06/01/2023
 #
 # Description: This script makes a copy of the network links and creates midpoints from these links. The link midpoints are
 #              then used to spatial join with the TAZ. The spatial join is then used to update the TAZID in the network links shapefile. Next
 #              the network nodes are spatial joined with the TAZ, then the spatil join in used to update the TAZID in the network nodes shapefile.
-# Requires: Geopandas
 #
-#
+# Requires:    Geopandas, Pandas
 
 # open cmd in this folder and run this command: C:\Users\cday\Anaconda3\python.exe 01_Update_HOT_gpd.py
 #C:\Users\cday\AppData\Local\ESRI\conda\envs\arcgispro-py3-geopandas\python.exe 01_Update_HOT_gpd.py
 
-# Start Script
 print("\nRunning Update HOTzone Python Script\n\n\n")
 
-# Import geopandas module
-print("Importing geopandas site package...\n")
+import sys, os, imp, time, traceback
 import geopandas as gpd
-import sys
-import os
-import time
-import traceback
 from pandas.api.types import is_numeric_dtype
 import importlib.machinery
 
 # Set file paths
+Pyth_root_dir = os.path.dirname(sys.argv[0])
 print("\n\nDefining variables...\n")
-#Pyth_root_dir = os.path.dirname(sys.argv[0])
-#print("Python root directory: \n    " + Pyth_root_dir + "\n")
-data = importlib.machinery.SourceFileLoader('data', r"_VARCUBE_UPDATEHOT.txt").load_module() #('data', os.path.join(Pyth_root_dir, r"_VarCube_UPDATEHOT.txt")).load_module()
+print("Python root directory: \n    " + Pyth_root_dir + "\n")
 
+# Get input variables from Cube
+get_variables = os.path.join(Pyth_root_dir, r"_VarCube_UPDATEHOT.txt")
+print("Cube variables input file: \n    " + get_variables + "\n\n")
 
-# Print key variables to screen
-print("tollz_shp: \n    "         + data.tollz_shp         + "\n")
-print("Scenario_Link: \n    "     + data.Scenario_Link     + "\n")
-print("Scenario_Node: \n    "     + data.Scenario_Node     + "\n")
-print("UsedZones: \n    "         + str(data.UsedZones)    + "\n")
-print("Temp Folder:  \n    "      + r'temp_gpd'            + "\n")  #data.temp_folder       + "\n")
+# Load file variables from input text file
+data = importlib.machinery.SourceFileLoader('data', os.path.join(Pyth_root_dir, r"_VarCube_UPDATEHOT.txt")).load_module()
 
-# Open log file
-temp_folder = r'temp_gpd' # delete in real script
-#temp_folder      = str(data.temp_folder)
-log = os.path.join(temp_folder,r'LogFile_UpdateHOT.txt')
+# Open and write to log file
+log = os.path.join(Pyth_root_dir, "LogFile_UPDATEHOT.txt")
 logFile = open(log, 'w')
 logFile.write(data.tollz_shp+'\n')
 logFile.write(data.Scenario_Link+'\n')
 logFile.write(data.Scenario_Node+'\n')
 logFile.write(str(data.UsedZones)+'\n')
-logFile.write(temp_folder+'\n')
 
+print("tollz_shp: \n    "         + data.tollz_shp         + "\n")
+print("Scenario_Link: \n    "     + data.Scenario_Link     + "\n")
+print("Scenario_Node: \n    "     + data.Scenario_Node     + "\n")
+print("UsedZones: \n    "         + str(data.UsedZones)    + "\n")
+print("Temp Folder:  \n    "      + data.temp_folder       + "\n")
 
-# Define Variables ------------------------------------------------------------------------------------
+## Define Variables 
 # Variables: Input
 tollz_shp         = str(data.tollz_shp)
 link_shp          = str(data.Scenario_Link)
 node_shp          = str(data.Scenario_Node)
 UsedZones         = str(data.UsedZones)
+temp_folder       = str(data.temp_folder)
 
 # Variables: Intermediate
 link_taz_sj       = os.path.join(temp_folder, "Link_TAZ_SJ_deleteTemp.shp")
@@ -67,13 +62,10 @@ out_node         = os.path.join(temp_folder, "C1_Node_HOT.shp")
 out_link_mp      = os.path.join(temp_folder, "C1_Link_Midponts.shp")
 
 
-# Define Functions ------------------------------------------------------------------------------------
 # Codeblock to calculate HOTzone node value
-def calcTAZID_Link(tazid, aField, bField, global_n):
-    if int(aField) <= int(global_n):
-        return aField
-    elif int(bField) <= int(global_n):
-        return bField
+def calctollzoneID_Node(tazid, node, global_n):
+    if int(node) <= int(global_n):
+        return 0
     else:
         return tazid
     
@@ -99,75 +91,86 @@ def fill_na_sedf(df_with_shape_column):
         raise Exception("Dataframe does not include 'geometry' column")
 
 
-# Geoprocessing steps ---------------------------------------------------------------------------------
-try:
-    print("\n\nRunning script...")
-    print("Start Time: " + time.strftime('%X %x %Z')+"\n")
+def Main():
+    try:
+        print("\n\nRunning script...")
+        starttime = time.strftime('%X %x %Z')
+        print("Start Time: " + time.strftime('%X %x %Z')+"\n")
 
-    # Tag Links With Toll Zone ID
-    print("\n\nImporting Highway Link data...")
-    gdf_link = gpd.read_file(link_shp)
+        #=============================
+        # Tag Links With Toll Zone ID
+        #=============================
 
-    print("\nCalculating Highway Link midpoint coord and updating X_MID & Y_MID fields...")
-    gdf_link["X_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).x
-    gdf_link["Y_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).y
+        print("\n\nImporting Highway Link data...")
+        gdf_link = gpd.read_file(link_shp)
 
-    print("\nMaking new point shapefile from Highway Link midpoint...")
-    gdf_midpoints = gdf_link.copy()
-    gdf_midpoints = gdf_midpoints.set_geometry(gdf_midpoints.geometry.centroid)
-    gdf_midpoints = fill_na_sedf(gdf_midpoints)
-    gdf_midpoints.to_file(out_link_mp)
+        print("\nCalculating Highway Link midpoint coord and updating X_MID & Y_MID fields...")
+        gdf_link["X_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).x
+        gdf_link["Y_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).y
 
-    print("\nSpatial joining Tollz_shp to Link midpoints (this may take a few minutes)...")
-    gdf_tollz = gpd.read_file(tollz_shp)
-    gdf_link_taz_sj = gpd.sjoin(gdf_midpoints, gdf_tollz, how="left", op="within")
-    gdf_link_taz_sj = fill_na_sedf(gdf_link_taz_sj)
-    gdf_link_taz_sj.to_file(link_taz_sj)    
+        print("\nMaking new point shapefile from Highway Link midpoint...")
+        gdf_midpoints = gdf_link.copy()
+        gdf_midpoints = gdf_midpoints.set_geometry(gdf_midpoints.geometry.centroid)
+        gdf_midpoints = fill_na_sedf(gdf_midpoints)
+        gdf_midpoints.to_file(out_link_mp)
 
-    print("\nUpdating Highway Link HOTZONE ID...\n")
-    df_link_taz_sf = gdf_link_taz_sj[['LINKID','EL_Zone']]
-    gdf_link_join = gdf_link.merge(df_link_taz_sf, how = 'left', on='LINKID')
-    gdf_link_join["HOT_ZONEID"] = gdf_link_join["EL_Zone"]
-    gdf_link_join = gdf_link_join.drop(columns='EL_Zone', axis=1)
-    gdf_link_join = fill_na_sedf(gdf_link_join)
-    gdf_link_join.to_file(out_link)
+        print("\nSpatial joining Tollz_shp to Link midpoints (this may take a few minutes)...")
+        gdf_tollz = gpd.read_file(tollz_shp)
+        gdf_link_taz_sj = gpd.sjoin(gdf_midpoints, gdf_tollz, how="left", op="within")
+        gdf_link_taz_sj = fill_na_sedf(gdf_link_taz_sj)
+        gdf_link_taz_sj.to_file(link_taz_sj)    
 
+        print("\nUpdating Highway Link HOTZONE ID...\n")
+        df_link_taz_sf = gdf_link_taz_sj[['LINKID','EL_Zone']]
+        gdf_link_join = gdf_link.merge(df_link_taz_sf, how = 'left', on='LINKID')
+        gdf_link_join["HOT_ZONEID"] = gdf_link_join["EL_Zone"]
+        gdf_link_join = gdf_link_join.drop(columns='EL_Zone', axis=1)
+        gdf_link_join = fill_na_sedf(gdf_link_join)
+        gdf_link_join.to_file(out_link)
 
-    # Tag Nodes With Toll Zone ID
-    print("\n\nImporting Highway Node data for joining toll zone purpose..")
-    gdf_node = gpd.read_file(node_shp)  
+        #===============================
+        # Tag Nodes With Toll Zone ID
+        #===============================
 
-    print("\nSpatial joining Tollzone shape to Highway Nodes (this may take a few minutes)...")
-    gdf_tollz = gpd.read_file(tollz_shp)
-    gdf_node_tollz = gpd.sjoin(gdf_node,gdf_tollz, how="left", op="within") 
+        print("\n\nImporting Highway Node data for joining toll zone purpose..")
+        gdf_node = gpd.read_file(node_shp)  
 
-    print("\nUpdating Highway Node TollzoneID...\n")
-    gdf_node_taz_sj = fill_na_sedf(gdf_node_tollz)
-    gdf_node_taz_sj.to_file(node_taz_sj)    
+        print("\nSpatial joining Tollzone shape to Highway Nodes (this may take a few minutes)...")
+        gdf_tollz = gpd.read_file(tollz_shp)
+        gdf_node_tollz = gpd.sjoin(gdf_node,gdf_tollz, how="left", op="within") 
 
-    gdf_node_taz_hot = gdf_node_tollz.copy()
-    gdf_node_taz_hot["HOT_ZONEID"] = gdf_node_taz_hot.apply(lambda row: calctollzoneID_Node(row["EL_Zone"], row["N"], UsedZones), axis=1) 
-    gdf_node_taz_hot = gdf_node_taz_hot.drop(columns={'index_right','OBJECTID', 'Name', 'EL_Zone', 'Shape_Leng', 'Shape_Area'})
-    gdf_node_taz_hot = fill_na_sedf(gdf_node_taz_hot)
-    gdf_node_taz_hot.to_file(out_node) 
+        print("\nUpdating Highway Node TollzoneID...\n")
+        gdf_node_taz_sj = fill_na_sedf(gdf_node_tollz)
+        gdf_node_taz_sj.to_file(node_taz_sj)    
 
-    # Finish Script
-    print("Script End Time: " + time.strftime('%X %x %Z'))
-    logFile.write("All Finished"+"\n")
-    logFile.write("Script End Time: " + time.strftime('%X %x %Z'))
-    logFile.close()
+        gdf_node_taz_hot = gdf_node_tollz.copy()
+        gdf_node_taz_hot["HOT_ZONEID"] = gdf_node_taz_hot.apply(lambda row: calctollzoneID_Node(row["EL_Zone"], row["N"], UsedZones), axis=1) 
+        gdf_node_taz_hot = gdf_node_taz_hot.drop(columns={'index_right','OBJECTID', 'Name', 'EL_Zone', 'Shape_Leng', 'Shape_Area'})
+        gdf_node_taz_hot = fill_na_sedf(gdf_node_taz_hot)
+        gdf_node_taz_hot.to_file(out_node) 
 
-except:
-    # Error Code if something doesn't work
-    print("*** There was an error running this script - Check output logfile.")
-    tb = sys.exc_info()[2]
-    tbinfo = traceback.format_tb(tb)[0]
-    pymsg = "\nPYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(sys.exc_info())
-    msgs = "\nGeopandas ERRORS:\n" + str(sys.exc_info()[1]) + "\n"
-    logFile.write(pymsg + "\n")
-    logFile.write(msgs + "\n")
-    sys.exit(1)
+        #===============
+        # Finish Script
+        #===============
 
+        print("Script End Time: " + time.strftime('%X %x %Z'))
+        logFile.write("All Finished"+"\n")
+        logFile.write("Script Start Time: " + starttime +"\n")
+        logFile.write("Script End Time: " + time.strftime('%X %x %Z'))
+        logFile.close()
+
+    except:
+        # Error Code if something doesn't work
+        print("*** There was an error running this script - Check output logfile.")
+        tb = sys.exc_info()[2]
+        tbinfo = traceback.format_tb(tb)[0]
+        pymsg = "\nPYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(sys.exc_info())
+        msgs = "\nGeopandas ERRORS:\n" + str(sys.exc_info()[1]) + "\n"
+        logFile.write(pymsg + "\n")
+        logFile.write(msgs + "\n")
+        sys.exit(1)
+
+Main()
 
 
 
--- Update Link Node TAZID/01_Update_Link_Node_TAZID_gpd.py
+++ Update Link Node TAZID/01_Update_Link_Node_TAZID_gpd.py
@@ -1,58 +1,56 @@
-# Author: Justin Culp coded and Andy Li added Toll zone functions
-# Date: 12/06/2016
+# Author: Justin Culp & Andy Li 
+# Updated by Josh Reynolds & Chris Day
+# Date Edited:        07/01/2023
 #
 # Description: This script makes a copy of the network links and creates midpoints from these links. The link midpoints are
 #              then used to spatial join with the TAZ. The spatial join is then used to update the TAZID in the network links shapefile. Next
 #              the network nodes are spatial joined with the TAZ, then the spatil join in used to update the TAZID in the network nodes shapefile.
 
-# Requires: ArcGIS 10.2 Desktop Basic
+# Requires: Geopandas, Pandas
 
 #C:\Users\cday\AppData\Local\ESRI\conda\envs\arcgispro-py3-geopandas\python.exe 01_Update_Link_Node_TAZID_gpd.py
 
-# Import geopandas module
 print("\nRunning Update HOTzone Python Script\n\n\n")
-print("Importing geopandas site package...\n")
-import geopandas as gpd
-import sys
-import os
-import imp
-import time
-import traceback
-import numpy as np
-from shapely.geometry import box
+
+import sys, os, imp, time, traceback
 import pandas as pd
+import geopandas as gpd
+import numpy as np
+import importlib.machinery
 
-TAZ_shp = r'inputs-04-14\\TAZ.shp'
-Scenario_Link = r'inputs-04-14\\C1_Link.shp'
-Scenario_Node = r'inputs-04-14\\C1_Node.shp'
-UsedZones = '3629'
-temp_folder = r'temp_gpd'
-log = os.path.join(temp_folder,r'LogFile_UpdateLinkNodeTAZID.txt')
+# Set file paths
+Pyth_root_dir = os.path.dirname(sys.argv[0])
+print("\n\nDefining variables...\n")
+print("Python root directory: \n    " + Pyth_root_dir + "\n")
 
-# Print key variabls to screen
-print("TAZ_shp: \n    "           + TAZ_shp           + "\n")
-print("Scenario_Link: \n    "     + Scenario_Link     + "\n")
-print("Scenario_Node: \n    "     + Scenario_Node     + "\n")
-print("UsedZones: \n    "         + str(UsedZones)    + "\n")
-print("Temp Folder:  \n    "      + temp_folder       + "\n")
+# Get input variables from Cube
+get_variables = os.path.join(Pyth_root_dir, r"_VARCUBE_UPDATETAZID.txt")
+print("Cube variables input file: \n    " + get_variables + "\n\n")
 
-# Open log file
+# Load file variables from input text file
+data = importlib.machinery.SourceFileLoader('data', os.path.join(Pyth_root_dir, r"_VARCUBE_UPDATETAZID.txt")).load_module()
+
+# Open and write to log file
+log = os.path.join(Pyth_root_dir, "LogFile_UpdateLinkNodeTAZID.txt")
 logFile = open(log, 'w')
-logFile.write(TAZ_shp+'\n')
-logFile.write(Scenario_Link+'\n')
-logFile.write(Scenario_Node+'\n')
-logFile.write(str(UsedZones)+'\n')
-logFile.write(temp_folder+'\n')
+logFile.write(data.TAZ_shp +'\n')
+logFile.write(data.Scenario_Link+'\n')
+logFile.write(data.Scenario_Node+'\n')
+logFile.write(str(data.UsedZones)+'\n')
 
-#raw_input()
+print("TAZ_shp: \n    "           + data.TAZ_shp           + "\n")
+print("Scenario_Link: \n    "     + data.Scenario_Link     + "\n")
+print("Scenario_Node: \n    "     + data.Scenario_Node     + "\n")
+print("UsedZones: \n    "         + str(data.UsedZones)    + "\n")
+print("Temp Folder:  \n    "      + data.temp_folder       + "\n")
 
 ## Define Variables
 # Variables: Input
-taz_shp           = str(TAZ_shp)
-link_shp          = str(Scenario_Link)
-node_shp          = str(Scenario_Node)
-UsedZones         = str(UsedZones)
-temp_folder       = str(temp_folder)
+taz_shp           = str(data.TAZ_shp)
+link_shp          = str(data.Scenario_Link)
+node_shp          = str(data.Scenario_Node)
+UsedZones         = str(data.UsedZones)
+temp_folder       = str(data.temp_folder)
 
 # Variables: Intermediate
 link_taz_sj       = os.path.join(temp_folder, "Link_TAZ_SJ_deleteTemp.shp")
@@ -63,7 +61,8 @@ out_link          = os.path.join(temp_folder, "C1_Link_TAZID.shp")
 out_node          = os.path.join(temp_folder, "C1_Node_TAZID.shp")
 out_link_mp       = os.path.join(temp_folder, "C1_Link_Midponts.shp")
 
-# Codeblock to calculate TAZID
+
+# Codeblocks to calculate TAZID
 def calcTAZID_Node(tazid, node, global_n):
   if node <= int(global_n):
     return node
@@ -77,79 +76,89 @@ def calcTAZID_Link(tazid, aField, bField, global_n):
         return bField
     else:
         return tazid
-
-# Geoprocessing steps
-def TagLinksWithTAZID():
-    print("\n\nImporting Highway Link data...")
-    gdf_link = gpd.read_file(link_shp)
-
-    print("\nCalculating Highway Link distance (in miles) and updating DISTANCE field...")
-    gdf_link["DISTANCE"] = gdf_link.geometry.length / 1609.34
-
-    print("\nCalculating Highway Link midpoint coord and updating X_MID & Y_MID fields...")
-    gdf_link["X_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).x
-    gdf_link["Y_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).y
-
-    print("\nMaking new point shapefile from Highway Link midpoint...")
-    gdf_midpoints = gdf_link.copy()
-    gdf_midpoints = gdf_midpoints.set_geometry(gdf_midpoints.geometry.centroid)
-    gdf_midpoints.to_file(out_link_mp)
-
-    print("\nSpatial joining TAZ to Link midpoints (this may take a few minutes)...")
-    # read in taz shapefile and calculate which tazes are closest to each midpoint value
-    gdf_taz = gpd.read_file(taz_shp)
-    gdf_nearest = gpd.sjoin_nearest(gdf_midpoints,gdf_taz, distance_col = 'nearest_dist')
-
-    #drop duplicates where tazes are equidistant from the link midpoint by keeping the first occurence
-    second_occurences = gdf_nearest['LINKID'].duplicated(keep='first')
-    gdf_nearest_final = gdf_nearest[~second_occurences]
-    gdf_link_taz_sj = gdf_nearest_final.rename(columns={'TAZID_left':'TAZID','TAZID_right':'TAZID_1'})
-    gdf_link_taz_sj.to_file(link_taz_sj)
-
-    drop_columns = ['TAZID_V832', 'SORT', 'CO_IDX', 'CO_TAZID', 'SUBAREAID', 'ACRES', 'DEVACRES', 'DEVPBLEPCT', 'X', 'Y', 'ADJ_XY', 'CO_FIPS', 'CO_NAME', 'CITY_FIPS', 'CITY_UGRC', 'CITY_NAME', 'DISTSUPER', 'DSUP_NAME', 'DISTLRG', 'DLRG_NAME', 'DISTMED', 'DMED_NAME', 'DISTSML', 'DSML_NAME', 'CBD', 'TERMTIME', 'PRKCSTPERM', 'PRKCSTTEMP', 'WALK100', 'ECOEDPASS', 'FREEFARE', 'REMM']
     
-    print("\nUpdating Highway Link TAZID...\n")
-    gdf_link_mp = gdf_link_taz_sj.copy()
-    gdf_link_mp["TAZID"] = gdf_link_mp.apply(lambda row: calcTAZID_Link(row["TAZID_1"], row["A"], row["B"], UsedZones), axis=1)
-    gdf_link_mp = gdf_link_mp.drop(columns=drop_columns).drop(columns={'nearest_dist','TAZID_1'})
-    gdf_link_mp = gdf_link_mp.sort_values(by='LINKID', ascending=True)
-    gdf_link_mp.to_file(out_link)
-
-def TagNodesWithTAZID():
-    print("\n\nImporting Highway Node data...")
-    gdf_nodes = gpd.read_file(node_shp)
-
-    print("\nSpatial joining TAZ to Highway Nodes (this may take a few minutes)...")
-    # read in taz shapefile and calculate which tazes are closest to each midpoint value
-    gdf_taz = gpd.read_file(taz_shp)
-    gdf_nodes_nearest_taz = gpd.sjoin_nearest(gdf_nodes,gdf_taz, distance_col = 'nearest_dist')
-
-    #drop duplicates where tazes are equidistant from the link midpoint by keeping the first occurence
-    second_node_occurences = gdf_nodes_nearest_taz['N'].duplicated(keep='first')
-    gdf_node_nearest_final = gdf_nodes_nearest_taz[~second_node_occurences]
-    gdf_node_taz_sj = gdf_node_nearest_final.rename(columns={'X_left':'X','Y_left':'Y','TAZID_left':'TAZID','TAZID_right':'TAZID_1','X_right':'X_1','Y_right':'Y_1'})
-    gdf_node_taz_sj = gdf_node_taz_sj.drop(columns={'nearest_dist'})
-    gdf_node_taz_sj.to_file(node_taz_sj)
-     
-    print("\nUpdating Highway Node TAZID...\n")
-    gdf_node_mp = gdf_node_taz_sj.copy()
-    gdf_node_mp['TAZID'] = gdf_node_mp.apply(lambda row: calcTAZID_Node(row['TAZID_1'], row['N'], UsedZones), axis = 1)
-    gdf_node_mp = gdf_node_mp.iloc[:,:43]
-    gdf_node_mp.to_file(out_node)
-    
-
-
 def Main():
     try:
         print("\n\nRunning script...")
+        starttime = time.strftime('%X %x %Z')
         print("Start Time: " + time.strftime('%X %x %Z')+"\n")
-        TagLinksWithTAZID()
-        TagNodesWithTAZID()
+
+        #=====================
+        #Tag Links with TAZID
+        #=====================
+
+        print("\n\nImporting Highway Link data...")
+        gdf_link = gpd.read_file(link_shp)
+
+        print("\nCalculating Highway Link distance (in miles) and updating DISTANCE field...")
+        gdf_link["DISTANCE"] = gdf_link.geometry.length / 1609.34
+
+        print("\nCalculating Highway Link midpoint coord and updating X_MID & Y_MID fields...")
+        gdf_link["X_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).x
+        gdf_link["Y_MID"] = gdf_link.geometry.interpolate(0.5, normalized=True).y
+
+        print("\nMaking new point shapefile from Highway Link midpoint...")
+        gdf_midpoints = gdf_link.copy()
+        gdf_midpoints = gdf_midpoints.set_geometry(gdf_midpoints.geometry.centroid)
+        gdf_midpoints.to_file(out_link_mp)
+
+        print("\nSpatial joining TAZ to Link midpoints (this may take a few minutes)...")
+        # read in taz shapefile and calculate which tazes are closest to each midpoint value
+        gdf_taz = gpd.read_file(taz_shp)
+        gdf_nearest = gpd.sjoin_nearest(gdf_midpoints,gdf_taz, distance_col = 'nearest_dist')
+
+        #drop duplicates where tazes are equidistant from the link midpoint by keeping the first occurence
+        second_occurences = gdf_nearest['LINKID'].duplicated(keep='first')
+        gdf_nearest_final = gdf_nearest[~second_occurences]
+        gdf_link_taz_sj = gdf_nearest_final.rename(columns={'TAZID_left':'TAZID','TAZID_right':'TAZID_1'})
+        gdf_link_taz_sj.to_file(link_taz_sj)
+
+        drop_columns = ['TAZID_V832', 'SORT', 'CO_IDX', 'CO_TAZID', 'SUBAREAID', 'ACRES', 'DEVACRES', 'DEVPBLEPCT', 'X', 'Y', 'ADJ_XY', 'CO_FIPS', 'CO_NAME', 'CITY_FIPS', 'CITY_UGRC', 'CITY_NAME', 'DISTSUPER', 'DSUP_NAME', 'DISTLRG', 'DLRG_NAME', 'DISTMED', 'DMED_NAME', 'DISTSML', 'DSML_NAME', 'CBD', 'TERMTIME', 'PRKCSTPERM', 'PRKCSTTEMP', 'WALK100', 'ECOEDPASS', 'FREEFARE', 'REMM']
+
+        print("\nUpdating Highway Link TAZID...\n")
+        gdf_link_mp = gdf_link_taz_sj.copy()
+        gdf_link_mp["TAZID"] = gdf_link_mp.apply(lambda row: calcTAZID_Link(row["TAZID_1"], row["A"], row["B"], UsedZones), axis=1)
+        gdf_link_mp = gdf_link_mp.drop(columns=drop_columns).drop(columns={'nearest_dist','TAZID_1'})
+        gdf_link_mp = gdf_link_mp.sort_values(by='LINKID', ascending=True)
+        gdf_link_mp.to_file(out_link)
+
+        #=====================
+        #Tag Nodes with TAZID
+        #=====================
+
+        print("\n\nImporting Highway Node data...")
+        gdf_nodes = gpd.read_file(node_shp)
+
+        print("\nSpatial joining TAZ to Highway Nodes (this may take a few minutes)...")
+        # read in taz shapefile and calculate which tazes are closest to each midpoint value
+        gdf_taz = gpd.read_file(taz_shp)
+        gdf_nodes_nearest_taz = gpd.sjoin_nearest(gdf_nodes,gdf_taz, distance_col = 'nearest_dist')
+
+        #drop duplicates where tazes are equidistant from the link midpoint by keeping the first occurence
+        second_node_occurences = gdf_nodes_nearest_taz['N'].duplicated(keep='first')
+        gdf_node_nearest_final = gdf_nodes_nearest_taz[~second_node_occurences]
+        gdf_node_taz_sj = gdf_node_nearest_final.rename(columns={'X_left':'X','Y_left':'Y','TAZID_left':'TAZID','TAZID_right':'TAZID_1','X_right':'X_1','Y_right':'Y_1'})
+        gdf_node_taz_sj = gdf_node_taz_sj.drop(columns={'nearest_dist'})
+        gdf_node_taz_sj.to_file(node_taz_sj)
+
+        print("\nUpdating Highway Node TAZID...\n")
+        gdf_node_mp = gdf_node_taz_sj.copy()
+        gdf_node_mp['TAZID'] = gdf_node_mp.apply(lambda row: calcTAZID_Node(row['TAZID_1'], row['N'], UsedZones), axis = 1)
+        gdf_node_mp = gdf_node_mp.iloc[:,:43]
+        gdf_node_mp.to_file(out_node)
+
+        #===============
+        # Finish Script
+        #===============
+
         print("Script End Time: " + time.strftime('%X %x %Z'))
         logFile.write("All Finished"+"\n")
+        logFile.write("Script Start Time: " + starttime +"\n")
         logFile.write("Script End Time: " + time.strftime('%X %x %Z'))
         logFile.close()
+
     except:
+        # Error Code if something doesn't work
         print("*** There was an error running this script - Check output logfile.")
         tb = sys.exc_info()[2]
         tbinfo = traceback.format_tb(tb)[0]
@@ -160,7 +169,3 @@ def Main():
         sys.exit(1)
 
 Main()
-
-
-
-
